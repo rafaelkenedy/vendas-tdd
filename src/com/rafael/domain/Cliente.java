fd254deba306dcd93f6bebf0cb866fed0b2e6153
rafael <k.rafaelalves@gmail.com>
2023-08-23 11:33:07 -0300

    first

---
+++ src/com/rafael/domain/Cliente.java
@@ -0,0 +1,64 @@
+package com.rafael.domain;
+
+public class Cliente {
+
+	private String nome;
+	private String email;
+	private String telefone;
+	private String endereco;
+	private Long cpf;
+	
+	public Cliente() {
+		
+	}
+
+	public Cliente(String nome, String email, String telefone, String endereco, Long cpf) {
+		super();
+		this.nome = nome;
+		this.email = email;
+		this.telefone = telefone;
+		this.endereco = endereco;
+		this.cpf = cpf;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+
+	public String getEndereco() {
+		return endereco;
+	}
+
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+
+	public Long getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(Long cpf) {
+		this.cpf = cpf;
+	}
+
+}
---
+++ test/com/rafael/domain/ClienteTest.java
@@ -0,0 +1,12 @@
+package com.rafael.domain;
+
+import org.junit.Test;
+
+public class ClienteTest {
+	
+	@Test	
+	public void consultarCliente() {
+		
+	}
+
+}
